2e366ab2cc14a90e7de77c0ead00334b6b49905c
feedallcat <artmuk.for.game@gmail.com>
2025-07-29 01:16:13 +0700

    update PlayerCharacter !!!

--- Source/PracticePlatform2D/Features/Player/PlayerCharacter.cpp
+++ Source/PracticePlatform2D/Features/Player/PlayerCharacter.cpp
@@ -1,4 +1,98 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PlayerCharacter.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputComponent.h"
+#include "PaperFlipbookComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/PlayerController.h"
 
+APlayerCharacter::APlayerCharacter() {
+
+}
+
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController())) {
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer())) {
+			if (ImcDefault) {
+				Subsystem->AddMappingContext(ImcDefault, 0);
+			}
+			else {
+				UE_LOG(LogTemp, Warning, TEXT("ImcDefault is not set in APlayerCharacter::BeginPlay()"));
+			}
+		}
+	}
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent)) {
+		if (IaMove) {
+			EnhancedInputComponent->BindAction(IaMove, ETriggerEvent::Triggered, this, &APlayerCharacter::OnMoveInput);
+			EnhancedInputComponent->BindAction(IaMove, ETriggerEvent::Completed, this, &APlayerCharacter::OnMoveCompleted);
+		}
+		else {
+			UE_LOG(LogTemp, Warning, TEXT("IaMove is not set in APlayerCharacter::SetupPlayerInputComponent()"));
+		}
+		if (IaJump) {
+			EnhancedInputComponent->BindAction(IaJump, ETriggerEvent::Started, this, &APlayerCharacter::OnJumpInput);
+		}
+		else {
+			UE_LOG(LogTemp, Warning, TEXT("IaJump is not set in APlayerCharacter::SetupPlayerInputComponent()"));
+		}
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerInputComponent is not of type UEnhancedInputComponent in APlayerCharacter::SetupPlayerInputComponent()"));
+	}
+}
+
+void APlayerCharacter::OnMoveInput(const FInputActionValue& Value) {
+	if (bPressedJump) {
+		return;
+	}
+	float value = Value.Get<float>();
+	FVector vector = GetActorForwardVector();
+	AddMovementInput(vector, value);
+	GetSprite()->SetFlipbook(PfRun);
+
+	if (value >= 0.0f) {
+		GetSprite()->SetWorldRotation(FRotator(0.0f, 0.0f, 0.0f));
+	}
+	else {
+		GetSprite()->SetWorldRotation(FRotator(0.0f, 0.0f, -180.0f));
+	}
+}
+
+void APlayerCharacter::OnMoveCompleted(const FInputActionValue& Value) {
+	if (bPressedJump) {
+		return;
+	}
+	GetSprite()->SetFlipbook(PfIdle);
+}
+
+void APlayerCharacter::OnJumpInput(const FInputActionValue& Value) {
+	if (bPressedJump) {
+		return;
+	}
+	Jump();
+	GetSprite()->SetLooping(false);
+	GetSprite()->SetFlipbook(PfJump);
+
+	GetWorld()->GetTimerManager().SetTimer(DelayTimerHandle, this, &APlayerCharacter::OnDelayComplete, 0.1f, false);
+}
+
+void APlayerCharacter::OnDelayComplete() {
+	if (GetVelocity().Z == 0) {
+		GetSprite()->SetLooping(false);
+		GetSprite()->Play();
+		GetSprite()->SetFlipbook(PfIdle);
+		StopJumping();
+	}
+	else {
+		GetWorld()->GetTimerManager().SetTimer(DelayTimerHandle, this, &APlayerCharacter::OnDelayComplete, 0.1f, false);
+	}
+}
--- Source/PracticePlatform2D/Features/Player/PlayerCharacter.h
+++ Source/PracticePlatform2D/Features/Player/PlayerCharacter.h
@@ -4,14 +4,52 @@
 
 #include "CoreMinimal.h"
 #include "PaperCharacter.h"
+#include "InputMappingContext.h"
+#include "PaperFlipbook.h"
+#include "PaperFlipbookComponent.h"
 #include "PlayerCharacter.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class PRACTICEPLATFORM2D_API APlayerCharacter : public APaperCharacter
 {
 	GENERATED_BODY()
-	
+
+public:
+	APlayerCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input")
+	UInputMappingContext* ImcDefault;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input")
+	UInputAction* IaMove;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	UInputAction* IaJump;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Sprite")
+	UPaperFlipbook* PfIdle;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Sprite")
+	UPaperFlipbook* PfRun;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Sprite")
+	UPaperFlipbook* PfJump;
+
+	void OnMoveInput(const FInputActionValue& Value);
+
+	void OnMoveCompleted(const FInputActionValue& Value);
+
+	void OnJumpInput(const FInputActionValue& Value);
+
+	FTimerHandle DelayTimerHandle;
+
+	void OnDelayComplete();
 };
